34c807ae022b7c3a79852594cda4444155294b6c
zeeforum <zartashzulfiqar@gmail.com>
2022-06-08 05:46:28 +0500

    Added Commands in Schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,15 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+
+        // Fetch Jobs From Indeed
+        $schedule->command('scrapper:indeed --query=php --fromage=7')->hourly();
+        $schedule->command('scrapper:indeed --query=php --fromage=7 --country=pk')->hourlyAt(5);
+        $schedule->command('scrapper:indeed --query=laravel --fromage=7')->hourlyAt(20);
+        $schedule->command('scrapper:indeed --query=laravel --fromage=7 --country=pk')->hourlyAt(40);
+
+        // Fetch Job Details from Indeed
+        $schedule->command('indeed:jobdetail')->everyFiveMinutes();
     }
 
     /**
